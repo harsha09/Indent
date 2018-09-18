df9359e9a22b86a30e93ab19a7111ecaf8a54871
Harsha Bharadwaj <harsha.bajju@gmail.com>
2018-09-18 07:54:48 +0530

    documenting

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ class IndexGenerator(object):
         self.outline = []
         self.text = text.readlines()
         self.output = []
+        # stack ds to generate +/- index. It is used to replace previous line's - with +
         self.stack = []
         self.output_file_path = output_file_path
 
@@ -23,6 +24,11 @@ class IndexGenerator(object):
 
 
     def get_indent_level(self, splitted_txt):
+        """ :args: splitted_txt: list
+            Converts the lines to index.
+            if starts with * then numbering 
+            if starts with . then +/-
+            if it doesn't include both considering same line continuation with previous line"""
         indent_len = len(splitted_txt[0])
         outline_len = len(self.outline)
         indentation = ''
@@ -35,6 +41,7 @@ class IndexGenerator(object):
             previous_list = []
             prev_indent = previous[0]
             
+            # if line not starting with * or . then consider it as continuation line with previous line.
             if indent_len == 0:
                 previous_1 = '{}\n{}{}'.format(
                     previous[1], ' '*len(prev_indent), splitted_txt[1])
@@ -42,18 +49,25 @@ class IndexGenerator(object):
                 self.stack.append(previous)
                 return
 
+            # if previous line indent has - and current line starts with .
             if ('-' in prev_indent) and ('.' in splitted_txt[0]):
+                # replace previous line - with + if current line is under previous line
                 operator = '+' if len(prev_indent) < (indent_len + 1) else '-'
                 prev_indent = prev_indent.replace('-', operator)
 
             self.stack.append((prev_indent, previous[1]))
 
+        # if line starting with . index with + or -
         if '.' in splitted_txt[0]:
             indentation = '{}{}'.format(' '*indent_len, '-')
 
+        # if line starting with * index with numbers
         if '*' in splitted_txt[0]:
+            # if current line stars len is greater than previous line then append 1.
             if outline_len < indent_len:
                 self.outline.append(1)
+            # if current line no of stars less than previous line
+            # no of stars then increment previous number by 1.
             elif outline_len > indent_len:
                 self.outline = self.outline[:indent_len]
                 self.outline[-1] += 1
@@ -66,6 +80,7 @@ class IndexGenerator(object):
 
 
     def generate(self):
+        """Writes to file."""
         for i in self.text:
             splitted_txt = self.split(i)
             if splitted_txt:
